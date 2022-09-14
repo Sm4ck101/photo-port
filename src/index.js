d699356aba16e33a771fd1754d7bf3d0d0c45a70
Sm4ck <mikeamartinez314@gmail.com>
2022-09-14 16:46:00 -0500

    Initialize a React Application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = document.getElementById("root");
 render(<App />, root);
